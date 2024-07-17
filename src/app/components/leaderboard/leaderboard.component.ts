c8bb3183ac3a491101e0cfc486b651f9f8c1bfb4
William Ambriz <Wambriz@me.com>
2024-07-16 19:27:49 -0700

    created leaderboard page with dummy data from JSON file

---
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { LeaderboardService } from '../../../services/leaderboard.service';
+
+interface LeaderboardEntry {
+  name: string
+  score: number
+}
+
+@Component({
+  selector: 'app-leaderboard',
+  templateUrl: './leaderboard.component.html',
+  styleUrls: ['./leaderboard.component.css']
+})
+export class LeaderboardComponent implements OnInit {
+  leaderboardEntries: LeaderboardEntry[] = []
+  constructor(private leaderboardService: LeaderboardService) { }
+
+  ngOnInit(): void {
+    this.leaderboardService.getLeaderboardEntries().subscribe(data => {
+      this.leaderboardEntries = data
+    })
+  }
+
+}
